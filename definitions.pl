26c98df257f422d24b26855bdd59e72bd09bf572
powcoder代写 <powcoder@163.com>
2022-07-07 14:01:36 +0800

    程序代写 加微信 powcoder

---
+++ definitions.pl
@@ -0,0 +1,8 @@
+/* ==========================================================================================================================
+Words we know
+*========================================================================================================================== */
+verbs([sings,eats,pets,found,saw,lifted]).
+nouns([apple,man,dog,alice,cow,book]).
+adjectives([green,lean,mean]).
+prepositions([of,at,with]).
+adverbs([quickly,carefully,brilliantly]).
